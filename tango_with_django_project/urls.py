0b443c69460826e5dcd023a1706551e0c71a9dd8
ZHixian <2815122Q@student.gla.ac.uk>
2023-01-22 22:23:06 +0000

    rango.urls

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rango import views
+from django.urls import include
+
 
 urlpatterns = [
+    path("", views.index, name="index"),
+    path("rango/", include("rango.urls")),
     path("admin/", admin.site.urls),
 ]
