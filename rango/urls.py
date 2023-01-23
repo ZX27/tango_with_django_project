e6ebf4f2f6f4ec8d65fd4738e3b72b78227b0509
ZHixian <2815122Q@student.gla.ac.uk>
2023-01-23 15:48:04 +0000

    about page

--- rango/urls.py
+++ rango/urls.py
@@ -3,5 +3,6 @@ from rango import views
 
 app_name = "rango"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
 ]
